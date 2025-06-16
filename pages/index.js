0633fecb63af13e0609d6ff658961567dba4986b
ubsramos <65291769+ubsramos@users.noreply.github.com>
2025-06-16 01:33:17 +0000

    all

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  const texto1 = "Escalas para Igrejas";
+  const texto2 = "Pregadores... Diáconos...Regência";
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>{texto1}</h1>
+      <p>{texto2}</p>
+    </div>
+  );
+}
+
+export default Home;
