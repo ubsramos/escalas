5de0dda4e8563e37cfbd5d15acb0a5827c0f9e02
ubsramos <65291769+ubsramos@users.noreply.github.com>
2025-06-19 00:56:58 +0000

    ajuestes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   const texto1 = "Escalas para Igrejas";
-  const texto2 = "Pregadores... Diáconos...Regência";
+  const texto2 = "Pregadores... Diáconos... Regência";
 
   return (
     <div style={{ textAlign: "center" ,
